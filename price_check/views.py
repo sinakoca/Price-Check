963116e001763af2362a0f1a93e4e726a6f84689
Volodymyr Floreskul <exufer@gmai.com>
2011-11-19 21:33:13 +0200

    Added geolocation

--- price_check/views.py
+++ price_check/views.py
@@ -8,6 +8,7 @@ from pymongo.objectid import ObjectId
 
 from stuff.product import Product
 from stuff.wish_list import WishList
+from stuff.geolocation import get_city
 
 import settings
 
@@ -24,12 +25,17 @@ def index(request):
     
 def search(request):
     query = request.GET.get('q') or ''
+    _check_session_wish_list(request.session)
+    wish_list = _get_wish_list(request.session)
+    _check_session_city(request.session)
+    city = _get_city(request.session)
     product_list = []
     relevant_productcs = products.find({'keywords' : query})
     for p in relevant_productcs:
         product = Product(p)
         product_list.append(product)
-    return render_to_response('search.html', {'query' : query, 'products' : product_list, 'wish_list' : _get_wish_list(request.session)})
+    return render_to_response('search.html', {'query' : query, 'products' : product_list, 'city' : city,
+        'wish_list' : wish_list})
     
 def add(request):
     product_id = request.GET.get('product_id')
@@ -40,20 +46,24 @@ def add(request):
     
     _check_session_wish_list(request.session)
     wish_list = _get_wish_list(request.session)
+    _check_session_city(request.session)
+    city = _get_city(request.session)
     if product_id:
         product_object = products.find_one({'_id' : ObjectId(product_id) })
         if product_object:
             product = Product(product_object)
             wish_list.add_product(product, quantity)
     request.session['wish_list'] = wish_list
-    return render_to_response('index.html', {'wish_list' : wish_list})
+    return render_to_response('index.html', {'city' : city, 'wish_list' : wish_list})
 
 def wish_list(request):
     product_id = request.GET.get('product_id')
     _check_session_wish_list(request.session)
     wish_list = _get_wish_list(request.session)
+    _check_session_city(request.session)
+    city = _get_city(request.session)
     
-    return render_to_response('list.html', {'wish_list' : wish_list})
+    return render_to_response('list.html', {'city' : city, 'wish_list' : wish_list})
     
 # def remove(request):
 #     product_id = request.GET.get('product_id')
@@ -74,10 +84,12 @@ def update(request):
     quantity = int(quantity_value)
     _check_session_wish_list(request.session)
     wish_list = _get_wish_list(request.session)
+    _check_session_city(request.session)
+    city = _get_city(request.session)
     
     wish_list.update_product(product_id, quantity)
     request.session['wish_list'] = wish_list
-    return render_to_response('index.html', {'wish_list' : wish_list})
+    return render_to_response('index.html', {'city' : city, 'wish_list' : wish_list})
 
 def compare(request):
     _check_session_wish_list(request.session)
@@ -103,8 +115,6 @@ def compare(request):
                 totals[i] += price * q
             prices.append(price)
         products_with_prices.append((p, q, prices))
-    pprint.pprint(products_with_prices)
-    print totals
     return render_to_response('compare.html', {'products_with_prices' : products_with_prices,
             'retailer_names' : retailer_names, 'totals' : totals})
 
@@ -117,9 +127,19 @@ def _get_wish_list(session):
 
 def _check_session_city(session):
     if 'city' not in session:
-        # TODO: replace
-        session['city'] = 'Tartu'
+        ip = get_client_ip(request)
+        # ip = '62.85.60.2'
+        city = get_city(ip) or 'Tartu'
+        session['city'] = city
+
 def _get_city(session):
     return session['city']
 
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
     
