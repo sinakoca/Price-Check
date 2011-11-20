25c846b24bad0ae86084a86ed379f89d20023816
Volodymyr Floreskul <exufer@gmai.com>
2011-11-20 12:22:02 +0200

    Fixed products sorting

--- price_check/stuff/wish_list.py
+++ price_check/stuff/wish_list.py
@@ -6,21 +6,26 @@ class WishList(object):
         self.created = datetime.now()
         self.last_modified = datetime.now()
     
+    def _sort_products(self):
+        self.products.sort(key=lambda x:x[0].name)
+    
     def add_product(self, product, quantity):
         for p, q in self.products:
             if p.id == product.id:
                 self.products.remove((p, q))
                 self.products.append((p, q + quantity))
+                self._sort_products()
                 return
         self.products.append((product, quantity))
         self.last_modified = datetime.now()
-        self.products.sort(key=lambda x:x[0].name)
+        self._sort_products()
 
     def remove_product(self, product_id):
         for p, q in self.products:
             if p.id == product_id:
                 self.products.remove((p, q))
                 break
+        self._sort_products()
     
     def update_product(self, product_id, quantity):
         for p, q in self.products:
@@ -29,3 +34,4 @@ class WishList(object):
                 if q > 0:
                     self.products.append((p, quantity))
                 break
+        self._sort_products()
